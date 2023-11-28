8801b5f350212a43b51a4c448e9e3daa9d3fc3c9
Gaik Artur <gaik.artur@yandex.ru>
2023-11-29 02:49:24 +0300

    Login design

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,8 @@
 import { type FC, useState } from 'react';
 import { currentUser as currentUserStore } from '@store/store';
 import { useLocation, useNavigate } from 'react-router-dom';
+import logo from './logo.png';
+import styles from './LoginPage.module.css';
 
 export const LoginPage: FC = () => {
   const [email, setEmail] = useState<string>('');
@@ -8,41 +10,83 @@ export const LoginPage: FC = () => {
   const [loginInProgress, setLoginInProgress] = useState<boolean>(false);
   const { state } = useLocation();
   const redirectBackUrl = state?.previousLocation?.pathname; // query parameters ???
+  const [message, setMessage] = useState<string | null>(null);
+  const [messageType, setMessageType] = useState<string | null>(null);
   const navigate = useNavigate();
+
   return (
-    <form
-      style={loginInProgress ? { opacity: 0.7, pointerEvents: 'none' } : {}}
-      onSubmit={(e) => {
-        e.preventDefault();
-        setLoginInProgress(true);
-        currentUserStore
-          .login(email, password)
-          .then((loginSucceeded) => {
-            if (loginSucceeded) navigate(redirectBackUrl);
-          })
-          .catch(() => {})
-          .finally(() => {
-            setLoginInProgress(false);
-          });
-      }}
-    >
-      <input
-        placeholder="Логин"
-        type="text"
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <input
-        placeholder="Пароль"
-        type="password"
-        value={password}
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <button type="submit">Логин</button>
-    </form>
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <img src={logo} alt="Росатом" className={styles.image} />
+        {message != null && (
+          <div
+            className={`${styles.message} ${
+              messageType === 'success'
+                ? styles.successMessage
+                : styles.errorMessage
+            }`}
+          >
+            {message}
+          </div>
+        )}
+        <form
+          style={loginInProgress ? { opacity: 0.7, pointerEvents: 'none' } : {}}
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (!loginInProgress) {
+              setLoginInProgress(true);
+              currentUserStore
+                .login(email, password)
+                .then((loginSucceeded) => {
+                  if (loginSucceeded) {
+                    setMessage('Вход выполнен успешно');
+                    setMessageType('success');
+                    setTimeout(() => {
+                      navigate(redirectBackUrl);
+                    }, 1000);
+                  } else {
+                    setMessage(
+                      'Произошла ошибка при аутентификации. Пожалуйста, попробуйте еще раз.',
+                    );
+                    setMessageType('error');
+                  }
+                })
+                .catch((error) => {
+                  console.error('Ошибка при аутентификации:', error);
+                })
+                .finally(() => {
+                  setLoginInProgress(false);
+                });
+            }
+          }}
+        >
+          <input
+            placeholder="Логин"
+            type="text"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className={styles.input}
+          />
+          <input
+            placeholder="Пароль"
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className={styles.input}
+          />
+          <div className={styles.checkbox}>
+            <input type="checkbox" id="rememberUser" />
+            <label htmlFor="rememberUser">Запомнить меня</label>
+          </div>
+          <button type="submit" className={styles.button}>
+            Войти
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
