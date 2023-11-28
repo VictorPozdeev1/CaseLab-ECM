f2d74e94313821826e48617103a3b7bbb8f3bd94
Gaik Artur <gaik.artur@yandex.ru>
2023-11-28 08:18:07 +0300

    auth component

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,35 +1,105 @@
-import { type FC, useState } from 'react';
-import store from '@store/store';
+import React, {
+  type FC,
+  useState,
+  type ChangeEvent,
+  type MouseEvent,
+} from 'react';
+// import store from '@store/store';
+import styles from './LoginPage.module.css';
+import { useNavigate } from 'react-router-dom';
+import { CaselabEcmApi } from '@api/CaselabEcmApi';
+import logo from './logo.png';
 
 export const LoginPage: FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [message, setMessage] = useState<string | null>(null);
+  const [messageType, setMessageType] = useState<string | null>(null);
+  const [rememberUser, setRememberUser] = useState(false);
+  const navigate = useNavigate();
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleRememberUserChange = () => {
+    setRememberUser(!rememberUser);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleLogin = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    try {
+      await CaselabEcmApi.loginService(email, password);
+
+      setMessage('Вход выполнен успешно');
+      setMessageType('success');
+      setTimeout(() => {
+        navigate('/');
+      }, 1000);
+    } catch (error) {
+      setMessage(
+        'Произошла ошибка при аутентификации. Пожалуйста, попробуйте еще раз.',
+      );
+      setMessageType('error');
+    }
+  };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        store.login(email, password).catch(() => {});
-        console.log(`Auth - ${store.isAuth}`);
-      }}
-    >
-      <input
-        placeholder="Логин"
-        type="text"
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <input
-        placeholder="Пароль"
-        type="password"
-        value={password}
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <button type="submit">Логин</button>
-    </form>
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <img src={logo} alt="Росатом" className={styles.image} />
+        {message != null && (
+          <div
+            className={`${styles.message} ${
+              messageType === 'success'
+                ? styles.successMessage
+                : styles.errorMessage
+            }`}
+          >
+            {message}
+          </div>
+        )}
+        <form>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              // eslint-disable-next-line no-lone-blocks
+              {
+                setEmail(e.target.value);
+              }
+            }}
+            className={styles.input}
+          />
+          <input
+            type="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              // eslint-disable-next-line no-lone-blocks
+              {
+                setPassword(e.target.value);
+              }
+            }}
+            className={styles.input}
+          />
+
+          <div className={styles.checkbox}>
+            <input
+              type="checkbox"
+              id="rememberUser"
+              checked={rememberUser}
+              onChange={handleRememberUserChange}
+            />
+            <label htmlFor="rememberUser">Запомнить меня</label>
+          </div>
+
+          {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+          <button onClick={handleLogin} className={styles.button}>
+            Войти
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
