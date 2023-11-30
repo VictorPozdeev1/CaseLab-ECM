8018832214d2612158ebd9921e06843bfe51da79
Victor Pozdeev <victorpozdeev@gmail.com>
2023-11-30 22:59:49 +0400

    Small refactoring in the root components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from '@app/App';
 
@@ -11,9 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
 
