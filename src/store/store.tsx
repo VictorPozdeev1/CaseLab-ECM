ad1cea850952357bee23de7e0c65662453fbd381
Alex <morgunowaleks@uandex.ru>
2023-12-01 00:40:15 +0300

    add function getAuthInfo and add current userData in store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,8 +1,9 @@
 import { makeAutoObservable, runInAction } from 'mobx';
 
 import CaselabEcmApi from '@api/CaselabEcmApi';
+import type IUser from '@entities/IUser';
 
-const { loginService } = CaselabEcmApi;
+const { loginService, getAuthInfo } = CaselabEcmApi;
 
 const TOKEN_ITEM_NAME = 'token';
 
@@ -14,17 +15,21 @@ class CurrentUser {
 
   isAuth: boolean = false;
   roles: string[] = [];
+  token: string = localStorage.getItem(TOKEN_ITEM_NAME) ?? '';
+  data: IUser | null = null;
 
   refreshState(): void {
     this.isAuth = localStorage.getItem(TOKEN_ITEM_NAME) !== null;
     this.roles = this.isAuth ? ['COMPANY_ADMIN', 'USER'] : [];
+    void getAuthInfo(this.token).then(
+      (dataRes: IUser) => (this.data = dataRes),
+    );
   }
 
   async login(email: string, password: string): Promise<boolean> {
     try {
-      // email = password; // for eslint
       const response = await loginService(email, password);
-      // const response = 'fake_token';
+      this.token = response;
       localStorage.setItem(TOKEN_ITEM_NAME, response);
       runInAction(() => {
         this.refreshState();
