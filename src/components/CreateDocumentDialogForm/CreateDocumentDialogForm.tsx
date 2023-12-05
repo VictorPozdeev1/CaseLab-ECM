b6bdd8005f8bcc76b34669f17e0a4950ed7e76e2
VictorPozdeev1 <114474092+VictorPozdeev1@users.noreply.github.com>
2023-12-05 11:21:51 +0400

    Some development in CreateDocumentForm (#28)
    * Install DataGrid
    
    * Add simplest stub of OwnDocumentsTable
    
    * WIP/ Create the base of CreateDocumentDialogForm
    
    * WIP / Add inputs
    
    * WIP
    
    * Add docTypes loading
    
    * Fix

---
+++ src/components/CreateDocumentDialogForm/CreateDocumentDialogForm.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect } from 'react';
+import type { FC } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Box,
+  TextField,
+  Select,
+  MenuItem,
+  type SelectChangeEvent,
+  //  Typography,
+} from '@mui/material';
+
+import styles from './CreateDocumentDialogForm.module.css';
+
+import { attributesStore, docTypesStore } from '@store/index';
+import { observer } from 'mobx-react-lite';
+
+interface CreateDocumentFormProps {
+  onSubmit: (
+    attributeValues: Array<{ attributeId: number; attributeValue: string }>,
+  ) => void;
+  onCancel: () => void;
+}
+
+export const CreateDocumentDialogForm: FC<CreateDocumentFormProps> = observer(
+  ({ onSubmit, onCancel }) => {
+    const [docType, setDocType] = React.useState('');
+
+    const handleDocTypeChange = (event: SelectChangeEvent): void => {
+      setDocType(event.target.value);
+    };
+
+    // todo это надо как-то переделать по-человечески
+    useEffect(() => {
+      void attributesStore.getAttributes();
+      void docTypesStore.getAllDocTypes();
+    }, []);
+
+    return (
+      <Dialog
+        className={styles.dialog}
+        PaperProps={{ variant: 'elevation', className: styles.paper }}
+        maxWidth={'md'}
+        open={true}
+        onClose={() => {
+          console.log(
+            'Пока что этот диалог сделан модальным, поэтому по клику по бэкдропу ничего не происходит.',
+          );
+        }}
+      >
+        <DialogTitle variant="h6" className={styles.title}>
+          Создать документ
+        </DialogTitle>
+        <DialogContent sx={{ alignSelf: 'stretch' }}>
+          <Box className={styles.inputGroup}>
+            <TextField
+              className={styles.documentNameInput}
+              label="Название"
+              variant="outlined"
+            />
+            <Select
+              className={styles.documentTypeSelect}
+              value={docType}
+              label="Тип документа"
+              onChange={handleDocTypeChange}
+            >
+              {docTypesStore.docTypes?.map((t) => (
+                <MenuItem key={t.id} value={t.id}>
+                  {t.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </Box>
+          <Box>
+            {attributesStore.attributes?.map((a) => (
+              <div key={a.id}>{a.name}</div>
+            ))}
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              console.log('cancel clicked');
+              onCancel();
+            }}
+          >
+            Отменить
+          </Button>
+          <Button
+            onClick={() => {
+              console.log('ok clicked');
+              onSubmit([
+                { attributeId: 1, attributeValue: 'a1' },
+                { attributeId: 2, attributeValue: 'dsjfh' },
+              ]);
+            }}
+          >
+            Создать
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  },
+);
