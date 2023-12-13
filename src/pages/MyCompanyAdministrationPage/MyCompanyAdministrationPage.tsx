24a0d452af17c9f02e0db9ff0aef4a8acbdd92c7
Victor Pozdeev <victorpozdeev@gmail.com>
2023-12-14 03:22:34 +0400

    Add blanks for company-users-administration pages

---
+++ src/pages/MyCompanyAdministrationPage/MyCompanyAdministrationPage.tsx
@@ -0,0 +1,11 @@
+import { type FC } from 'react';
+
+import { CompanyUsersAdministration } from '@widgets/CompanyUsersAdministration/CompanyUsersAdministration';
+import sessionStore from '@store/session';
+import Spinner from '@shared/components/Spinner/Spinner';
+
+export const MyCompanyAdministrationPage: FC = () => {
+  const companyId = sessionStore.userData?.organizationId;
+  if (typeof companyId !== 'number') return <Spinner />; // Или не принимать undefined?
+  return <CompanyUsersAdministration companyId={companyId} />;
+};
