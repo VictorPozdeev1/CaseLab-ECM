efe67a8925dce478f0fd8b973b93b55df250f858
zsaveleva <zsaveleva>
2023-12-14 19:55:39 +0300

    Update CompanyUsersAdministrationPage

--- src/widgets/CompanyUsersAdministration/CompanyUsersAdministration.tsx
+++ src/widgets/CompanyUsersAdministration/CompanyUsersAdministration.tsx
@@ -3,17 +3,50 @@ import { type FC } from 'react';
 // import { currentUserStore } from '@store/index';
 // import { usersStore } from '@store/index';
 import { observer } from 'mobx-react-lite';
+import { Box, Button, Container, Typography } from '@mui/material';
+import { AddRounded } from '@mui/icons-material';
+import { UsersTable } from './UsersTable';
 
 export const CompanyUsersAdministration: FC<{ companyId: number }> = observer(
   ({ companyId }) => {
     return (
-      <div>
-        <h2>
-          Это страница пользователей, относящихся к организации с id={companyId}
-        </h2>
-        Тут будет список пользователей компании, где можно редачить их, задавать
-        пароль, итд.
-      </div>
+      <Container maxWidth={'lg'}>
+        <Box padding={'16px'}>
+          <Box
+            marginBottom={'28px'}
+            display={'flex'}
+            alignItems={'center'}
+            gap={'16px'}
+          >
+            <Typography variant="h5" fontWeight={'bold'}>
+              Список пользователей организации {companyId}
+            </Typography>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{
+                textTransform: 'none',
+                width: '40px',
+                height: '40px',
+                boxShadow: 'none',
+                borderRadius: '50%',
+                padding: '0 !important',
+                minWidth: '40px',
+                ':hover': {
+                  boxShadow: 'none',
+                },
+                ':disabled': {
+                  boxShadow: 'none',
+                },
+              }}
+            >
+              <AddRounded />
+            </Button>
+          </Box>
+          <UsersTable />
+        </Box>
+      </Container>
     );
   },
 );
