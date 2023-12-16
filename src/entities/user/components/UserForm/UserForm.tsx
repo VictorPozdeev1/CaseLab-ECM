0d21dcdf92084592b9b9396fe7a448f811e7b3d3
Vladislave Mungin <natalya.mungina9@mail.ru>
2023-12-16 23:07:00 +0300

    add UserForm

--- src/entities/user/components/UserForm/UserForm.tsx
+++ src/entities/user/components/UserForm/UserForm.tsx
@@ -1,22 +1,251 @@
-import { type FC, useState } from 'react';
+import { type Dispatch, type SetStateAction, type FC } from 'react';
+import styles from './UserForm.module.css';
+import { Service, type UserReplyDto, type UserUpdateDto } from '@api';
+import { observer } from 'mobx-react-lite';
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  TextField,
+  Typography,
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
-import { type User } from '../../User';
-
-interface UserFormProps {
-  user: User;
-  onSave: (user: User) => void;
+interface EditingUserFormProps {
+  userInfo: UserReplyDto;
+  setUserInfo: Dispatch<SetStateAction<UserReplyDto>>;
+  onCancel: () => void;
 }
 
-export const UserForm: FC<UserFormProps> = ({ user, onSave }) => {
-  const [userState, setUserState] = useState<User>(user); // eslint-disable-line
-  return (
-    <div>
-      {userState.email}
-      <button
-        onClick={() => {
-          onSave(userState);
-        }}
-      ></button>
-    </div>
-  );
-};
+export const UserForm: FC<EditingUserFormProps> = observer(
+  ({ userInfo, onCancel, setUserInfo }) => {
+    return (
+      <Dialog open={true} sx={{ width: '688px', margin: '0 auto' }}>
+        <DialogActions>
+          <CloseIcon
+            color="action"
+            onClick={() => {
+              onCancel();
+            }}
+          />
+        </DialogActions>
+        <DialogContent
+          sx={{
+            borderTop: '1px solid #344054',
+            borderBottom: '1px solid #344054',
+          }}
+        >
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>ФИО*</Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              onChange={(e) => {
+                userInfo.fullName = e.target.value;
+
+                setUserInfo(userInfo);
+              }}
+              defaultValue={userInfo.fullName}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>Дата рождения</Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              onChange={(e) => {
+                userInfo.dateOfBirth = e.target.value;
+
+                setUserInfo(userInfo);
+              }}
+              defaultValue={userInfo.dateOfBirth}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>email</Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              defaultValue={userInfo.email}
+              onChange={(e) => {
+                userInfo.email = e.target.value;
+
+                setUserInfo(userInfo);
+              }}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}> Оргинизация </Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              defaultValue={userInfo.orgDto?.name}
+              onChange={(e) => {
+                if (userInfo.orgDto !== undefined) {
+                  userInfo.orgDto.name = e.target.value;
+                }
+
+                setUserInfo(userInfo);
+              }}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>Телефон</Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              defaultValue={userInfo.phone}
+              onChange={(e) => {
+                userInfo.phone = e.target.value;
+
+                setUserInfo(userInfo);
+              }}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>Должность</Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              defaultValue={userInfo.post}
+              onChange={(e) => {
+                userInfo.post = e.target.value;
+
+                setUserInfo(userInfo);
+              }}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>Роль</Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              defaultValue={userInfo.role}
+              onChange={(e) => {
+                userInfo.role = e.target.value;
+
+                setUserInfo(userInfo);
+              }}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>
+              Паспорт серия/номер
+            </Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              defaultValue={
+                userInfo.userPassportDto?.passportSeries +
+                ' ' +
+                userInfo.userPassportDto?.passportNumber
+              }
+              onChange={(e) => {
+                if (userInfo.userPassportDto !== undefined) {
+                  userInfo.userPassportDto.passportSeries =
+                    e.target.value.split(' ')[0];
+                  userInfo.userPassportDto.passportNumber =
+                    e.target.value.split(' ')[1];
+                }
+
+                setUserInfo(userInfo);
+              }}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>Дата выдачи</Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              defaultValue={userInfo.userPassportDto?.passportDate}
+              onChange={(e) => {
+                if (userInfo.userPassportDto !== undefined) {
+                  userInfo.userPassportDto.passportDate = e.target.value;
+                }
+                setUserInfo(userInfo);
+              }}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>Код подразделения</Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              defaultValue={userInfo.userPassportDto?.passportKp}
+              onChange={(e) => {
+                if (userInfo.userPassportDto !== undefined) {
+                  userInfo.userPassportDto.passportKp = e.target.value;
+                }
+                console.log(userInfo.userPassportDto?.passportKp);
+                setUserInfo(userInfo);
+              }}
+            />
+          </Box>
+          <Box className={styles.inputBox}>
+            <Typography className={styles.label}>Паспорт выдан</Typography>
+            <TextField
+              sx={{ width: '446px' }}
+              defaultValue={userInfo.userPassportDto?.passportIssued}
+              onChange={(e) => {
+                if (userInfo.userPassportDto !== undefined) {
+                  userInfo.userPassportDto.passportIssued = e.target.value;
+                }
+
+                setUserInfo(userInfo);
+              }}
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Button
+            onClick={() => {
+              onCancel();
+            }}
+            className={styles.button}
+          >
+            Отменить
+          </Button>
+          <Button
+            variant="contained"
+            className={styles.button}
+            onClick={() => {
+              const newUserInfo: UserUpdateDto = {};
+              newUserInfo.lastName = userInfo.fullName?.split(' ')[2];
+              newUserInfo.firstName = userInfo.fullName?.split(' ')[0];
+              newUserInfo.patronymic = userInfo.fullName?.split(' ')[1];
+              newUserInfo.dateOfBirth = userInfo.dateOfBirth;
+              newUserInfo.email = userInfo.email;
+              newUserInfo.phone = userInfo.phone;
+              newUserInfo.passportSeries =
+                userInfo.userPassportDto?.passportSeries;
+              newUserInfo.passportNumber =
+                userInfo.userPassportDto?.passportNumber;
+              newUserInfo.passportIssued =
+                userInfo.userPassportDto?.passportIssued;
+              newUserInfo.passportDate = userInfo.userPassportDto?.passportDate;
+
+              newUserInfo.passportKp = userInfo.userPassportDto?.passportKp;
+              Service.getAllOrgs()
+                .then((res) => {
+                  res.forEach((org) => {
+                    if (org.name === userInfo.orgDto?.name) {
+                      newUserInfo.organizationId = org.id;
+                    }
+                  });
+                })
+                .catch((e) => {
+                  console.log(e);
+                });
+
+              newUserInfo.role = userInfo.role;
+              newUserInfo.post = userInfo.post;
+
+              console.log(newUserInfo);
+              Service.updateUser(userInfo.id as number, newUserInfo)
+                .then((res) => {
+                  console.log(res);
+                })
+                .catch((e) => {
+                  console.log(e);
+                });
+            }}
+          >
+            Подтвердить
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  },
+);
--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,5 +1,47 @@
-import { type FC } from 'react';
+import { Service, type UserReplyDto } from '@api';
+import { UserForm } from '@entities/user';
+import { observer } from 'mobx-react-lite';
+import { type FC, useState, useEffect } from 'react';
 
-export const AdminPage: FC = () => {
-  return <div>AdminPages</div>;
-};
+export const AdminPage: FC = observer(() => {
+  const [userEmail, setUserEmail] = useState<string>('');
+  const [isMinimized, setIsMinimized] = useState<boolean>(true);
+  const [userInfo, setUserInfo] = useState<UserReplyDto>([] as UserReplyDto);
+
+  useEffect(() => {
+    Service.getUserByEmail(userEmail)
+      .then((res) => {
+        setUserInfo(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, [userEmail]);
+
+  const handleUserEmail = (event: any): void => {
+    setUserEmail(event.target.value);
+  };
+
+  if (isMinimized)
+    return (
+      <>
+        <input type="text" onChange={handleUserEmail} value={userEmail} />
+        <button
+          onClick={() => {
+            setIsMinimized(false);
+          }}
+        >
+          Click
+        </button>
+      </>
+    );
+  return (
+    <UserForm
+      onCancel={() => {
+        setIsMinimized(true);
+      }}
+      userInfo={userInfo}
+      setUserInfo={setUserInfo}
+    />
+  );
+});
