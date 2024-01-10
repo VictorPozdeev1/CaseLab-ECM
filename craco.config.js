75b5b75e93bb342e7e41b9be5204dda04b4a42e9
Slumrag <mrgreenthecristal@yandex.ru>
2024-01-10 18:32:36 +0300

    refactor: configure aliases \w tsconfig-paths plugin

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,16 @@
-const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  webpack: {
-    alias: {
-      '@api': path.resolve(__dirname, 'src/shared/api'),
-      '@app': path.resolve(__dirname, 'src/app'),
-      '@features': path.resolve(__dirname, 'src/features'),
-      '@shared': path.resolve(__dirname, 'src/shared'),
-      '@entities': path.resolve(__dirname, 'src/entities'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@store': path.resolve(__dirname, 'src/store'),
-      '@widgets': path.resolve(__dirname, 'src/widgets'),
-      '@processes': path.resolve(__dirname, 'src/processes'),
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
+          return webpackConfig;
+        },
+      },
     },
-  },
+  ],
   devServer: {
     port: 3000,
   },
