e042ceb18b52d767a0fac1311648d17e510f5c68
Slumrag <mrgreenthecristal@yandex.ru>
2024-01-10 20:09:30 +0300

    feat: add localization to MUI

--- src/shared/themes/theme.ts
+++ src/shared/themes/theme.ts
@@ -1,12 +1,19 @@
 import { type Theme, createTheme } from '@mui/material';
-
+import { ruRU as dataruRu } from '@mui/x-data-grid';
+import { ruRU as pickersruRU } from '@mui/x-date-pickers/locales';
+import { ruRU as coreruRU } from '@mui/material/locale';
 const themeColors = {
   naviBlue: '#035FA2',
 };
-export const appTheme: Theme = createTheme({
-  palette: {
-    primary: {
-      main: themeColors.naviBlue, // action-blue
+export const appTheme: Theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: themeColors.naviBlue, // action-blue
+      },
     },
   },
-});
+  coreruRU,
+  pickersruRU,
+  dataruRu,
+);
