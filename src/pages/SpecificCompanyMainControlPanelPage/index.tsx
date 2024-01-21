38bd405de867c61b723ecbacc893b0e5c673d4fa
Gaik Artur <gaik.artur@yandex.ru>
2024-01-21 03:16:55 +0300

    Внёс исправления в sidebar и form

--- src/pages/SpecificCompanyMainControlPanelPage/index.tsx
+++ src/pages/SpecificCompanyMainControlPanelPage/index.tsx
@@ -1,7 +1,11 @@
 import { type FC } from 'react';
-
 import { SpecificCompanyMainControlPanel } from '@widgets/CompanyMainControlPanel';
+import { useParams } from 'react-router-dom';
 
 export const SpecificCompanyMainControlPanelPage: FC = () => {
+  const { companyId: companyIdString } = useParams();
+  const companyId = Number(companyIdString);
+  if (Number.isNaN(companyId))
+    return <div>Не существует организации с id = {companyIdString}</div>;
   return <SpecificCompanyMainControlPanel />;
 };
