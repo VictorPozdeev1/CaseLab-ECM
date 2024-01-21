470d5de4e17143f9514a539d4f34561bd078b779
Gaik Artur <gaik.artur@yandex.ru>
2024-01-21 08:24:09 +0300

    Добавлен функционал добавления и удаления компаний

---
+++ src/entities/company/model/CompaniesStore.ts
@@ -0,0 +1,35 @@
+import { makeAutoObservable, runInAction } from 'mobx';
+
+import { Service as api } from '@api';
+import { Company } from './Company';
+
+class CompaniesStore {
+  companies: Company[] = [];
+  constructor() {
+    makeAutoObservable(this, {});
+  }
+
+  async _loadCompanies(): Promise<void> {
+    // content должен быть required, как я понимаю
+    // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
+    const newCompaniesList = (await api.getAllOrgs()).content!.map(
+      (o) => new Company(o),
+    );
+    runInAction(() => {
+      this.companies = newCompaniesList;
+    });
+  }
+
+  getNameById(id: number): string | undefined {
+    return this.companies.find((c) => c.id === id)?.name;
+  }
+}
+
+let _companiesStore: CompaniesStore | undefined;
+export const getCompaniesStore = (): CompaniesStore => {
+  if (_companiesStore === undefined) {
+    _companiesStore = new CompaniesStore();
+    void _companiesStore._loadCompanies();
+  }
+  return _companiesStore;
+};
