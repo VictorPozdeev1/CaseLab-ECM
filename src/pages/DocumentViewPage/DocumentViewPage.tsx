827d8cf4f0e704a7e8aad92bf584ced307b51bdc
Slumrag <mrgreenthecristal@yandex.ru>
2024-01-23 23:00:03 +0300

    fix: fix incorrect document process loading

--- src/pages/DocumentViewPage/DocumentViewPage.tsx
+++ src/pages/DocumentViewPage/DocumentViewPage.tsx
@@ -2,17 +2,30 @@ import { useEffect, type FC } from 'react';
 import { Box } from '@mui/material';
 import { DocumentViewMain } from '@widgets/DocumentViewMain/DocumentViewMain';
 import { observer } from 'mobx-react-lite';
-import { documentViewPageStore } from './store';
+import { documentViewPageStore as pageStore } from './store';
+import { documentProcessesStore as processesStore } from '@entities/documentProcess';
 import { useNavigate, useParams } from 'react-router-dom';
+import { getCompaniesStore } from '@entities/company/model';
+import { usersStore } from '@entities/user';
 
 export const DocumentViewPage: FC = observer(() => {
   const { documentName } = useParams();
   const navigate = useNavigate();
+  const companiesStore = getCompaniesStore();
   useEffect(() => {
-    void documentViewPageStore.loadDocumentByName(documentName as string);
-    if (documentViewPageStore.document === undefined) {
-      navigate('/myDocuments');
-    }
+    void (async () => {
+      void (await pageStore.loadDocumentByName(documentName as string));
+      if (pageStore.document !== undefined) {
+        await processesStore.load(pageStore.document.id);
+      } else {
+        navigate('/myDocuments');
+      }
+    })();
+    void companiesStore.loadCompanies();
+    void usersStore.loadOwnCompanyUsers();
+    return () => {
+      processesStore.clear();
+    };
   }, []);
 
   return (
